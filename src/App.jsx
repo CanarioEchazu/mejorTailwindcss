34b9bff71c87613fb8edcbea642564ba2c1fe467
CanarioEchazu <darioechazu@hotmail.com>
2023-03-24 16:32:47 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState} from 'react';
+import HelloWorld from './components/HelloWorld.jsx';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
